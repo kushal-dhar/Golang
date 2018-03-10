e9d438f588338e99e5942615456e896cb07fd61e
Kushal Dhar <kushal.dhar01@gmail.com>
2018-03-10 01:27:05 -0500

    Learning fmt package

---
+++ fmtpackage3.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a int // Declare a variable of type int
+var b string = "Hello there"
+var c bool // Declare a variable of type bool
+var d bool = true
+
+func main() {
+	e := 10
+	f := "hello Kushal"
+	g := `Looks like you are having fun with golang. how's the language so far? :)`
+	fmt.Println(a)
+	fmt.Printf("%v\n",b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+}
+
