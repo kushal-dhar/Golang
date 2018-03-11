3f1aadf3437f7da3c214cae79fe08839f9c2139c
Kushal Dhar <kushal.dhar01@gmail.com>
2018-03-10 19:47:28 -0500

    Exploring iota and bit shifting

---
+++ 11_bitshifting.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	_  = iota	//iota = 0
+	kb = 1 << (iota * 10)	//iota = 1
+	mb = 1 << (iota * 10)	//iota = 2
+	gb = 1 << (iota * 10)	//iota = 3
+)
+
+func main() {
+	fmt.Printf("%d\t\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t\t%b\n", gb, gb)
+}
+
