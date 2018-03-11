06cc081428a0be0f055c3ad1595c77f7b5802431
Kushal Dhar <kushal.dhar01@gmail.com>
2018-03-11 13:53:58 -0400

    Slice length and capacity

---
+++ 18_slice_len_cap.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := make([]int, 10, 12)
+	fmt.Println(x[:])
+
+	x = append(x, 11)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x = append(x, 12)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x = append(x, 13)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+}
+
